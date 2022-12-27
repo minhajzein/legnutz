aa78152fc73842a006213194ef06f6de5392131f
minhajzein <109206665+minhajzein@users.noreply.github.com>
2022-12-27 16:02:10 +0530

    total amount

--- controllers/admin-cnt.js
+++ controllers/admin-cnt.js
@@ -3,95 +3,103 @@ const { render } = require('ejs')
 const { default: mongoose } = require('mongoose')
 const Admin = require('../models/adminSchema')
 const Category = require('../models/categorySchema')
+const Order = require('../models/orderSchema')
 const Product = require('../models/productSchema')
 
 module.exports = {
-    errPage : (res,req) => {
+    errPage: (res, req) => {
         res.render('admin/not-available')
     },
-    home : async (req,res) => {
-        try{
-            if(req.session.adminLoggedIn){
-                res.render('admin/home',{adminData:req.session.adminData})
-            }else{
+    home: async (req, res) => {
+        try {
+            if (req.session.adminLoggedIn) {
+                res.render('admin/home', { adminData: req.session.adminData })
+            } else {
                 res.redirect('/admin/login')
             }
-        }catch{
+        } catch {
             res.redirect("/admin/not-available")
         }
     },
-    login : async (req,res) => {
-        try{
-            res.render('admin/login',{err_msg:false})
-        }catch{
+    login: async (req, res) => {
+        try {
+            res.render('admin/login', { err_msg: false })
+        } catch {
             res.redirect("/admin/not-available")
         }
     },
-    postLogin : async (req,res) => {
-        try{
-            const adminData = await Admin.findOne({Email:req.body.Email})
-            if(adminData){
-                if(adminData.password==req.body.password){
+    postLogin: async (req, res) => {
+        try {
+            const adminData = await Admin.findOne({ Email: req.body.Email })
+            if (adminData) {
+                if (adminData.password == req.body.password) {
                     req.session.adminLoggedIn = true
                     req.session.adminData = adminData
                     res.redirect('/admin')
-                }else{
-                    res.render('admin/login',{
-                        err_msg:"Password is incorrect"
+                } else {
+                    res.render('admin/login', {
+                        err_msg: "Password is incorrect"
                     })
                 }
-            }else{
-                res.render('admin/login',{
-                    err_msg:'Entered Email is not valid'
+            } else {
+                res.render('admin/login', {
+                    err_msg: 'Entered Email is not valid'
                 })
             }
-        }catch{
+        } catch {
             res.redirect('/admin/not-available')
         }
     },
-    logout : (req,res) => {
+    logout: (req, res) => {
         req.session.adminLoggedIn = false
         res.redirect('/admin/login')
     },
-    categoryPage : async (req,res) => {
-        try{
+    categoryPage: async (req, res) => {
+        try {
             const categories = await Category.find()
-            res.render('admin/category',{categories,err_msg:false})
-        }catch{
+            res.render('admin/category', { categories, err_msg: false })
+        } catch {
             res.redirect('/admin/not-available')
         }
     },
-    addCategory : async (req,res) => {
-        try{
-            const existCategory = await Category.findOne({category:req.body.category})
-            if(existCategory){
+    addCategory: async (req, res) => {
+        try {
+            const existCategory = await Category.findOne({ category: req.body.category })
+            if (existCategory) {
                 const categories = await Category.find()
-                res.render('admin/category',{categories,err_msg:`${existCategory} is already existed`})
-            }else{
+                res.render('admin/category', { categories, err_msg: `${existCategory} is already existed` })
+            } else {
                 const categories = {
-                    category : req.body.category
+                    category: req.body.category
                 }
                 await Category.create(categories)
                 res.redirect('/admin/category-page')
             }
-        }catch{
+        } catch {
             res.redirect('/admin/not-available')
         }
     },
-    deleteCategory : async (req,res) => {
-        try{
+    deleteCategory: async (req, res) => {
+        try {
             const id = req.query.id
-            const category = await Category.findById({_id:id})
-            const listedIn = await Product.findOne({productCategory:category.category})
-            if(listedIn){
+            const category = await Category.findById({ _id: id })
+            const listedIn = await Product.findOne({ productCategory: category.category })
+            if (listedIn) {
                 const categories = await Category.find()
-                res.render('admin/category',{categories,err_msg : `Cannot delete ${category.category} category, some products still in this category`})
-            }else{
-                await Category.deleteOne({_id:id})
+                res.render('admin/category', { categories, err_msg: `Cannot delete ${category.category} category, some products still in this category` })
+            } else {
+                await Category.deleteOne({ _id: id })
                 res.redirect('/admin/category-page')
             }
-        }catch{
+        } catch {
             res.redirect('/admin/not-available')
+        }
+    },
+    orderManage: async (req, res) => {
+        try {
+            const orders = await Order.find()
+        } catch {
+
         }
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -2,46 +2,47 @@ const express = require('express')
 const router = express.Router()
 const userControl = require('../controllers/user-cnt')
 const control = require('../controllers/admin-cnt')
-const checkSession = require ('../middleware/session') 
+const checkSession = require('../middleware/session')
 const productControl = require('../controllers/product-cnt')
 const fileupload = require('../middleware/multer')
 
 //==========================================================================================
 
-router.get('/',control.home)
+router.get('/', control.home)
 
+router.get('orderManage', control.orderManage)
 
 router.route('/login')
     .get(control.login)
     .post(control.postLogin)
 
-router.get('/users-list',userControl.usersList)
+router.get('/users-list', userControl.usersList)
 
-router.get('/block-user',userControl.blockUser)
+router.get('/block-user', userControl.blockUser)
 
-router.get('/unBlock-user',userControl.unBlockUser)
+router.get('/unBlock-user', userControl.unBlockUser)
 
 router.route('/addProduct')
     .get(productControl.addProduct)
-    .post(fileupload.uploadImages,fileupload.resizeImages,productControl.postAddproduct)
+    .post(fileupload.uploadImages, fileupload.resizeImages, productControl.postAddproduct)
 
-router.get('/productList',productControl.getProductList)
+router.get('/productList', productControl.getProductList)
 
 router.route('/editProduct')
     .get(productControl.editPage)
-    .post(fileupload.uploadImages,fileupload.resizeImages,productControl.editProduct)
-router.get('/category-page',control.categoryPage)
+    .post(fileupload.uploadImages, fileupload.resizeImages, productControl.editProduct)
+router.get('/category-page', control.categoryPage)
 
-router.get('/deleteProduct',productControl.deleteProduct)
+router.get('/deleteProduct', productControl.deleteProduct)
 
-router.post('/add-category',control.addCategory)
+router.post('/add-category', control.addCategory)
 
-router.get('/deleteCategory',control.deleteCategory)
+router.get('/deleteCategory', control.deleteCategory)
 
-router.get('/logout',control.logout)
+router.get('/logout', control.logout)
 
-router.get('/not-available',control.errPage)
+router.get('/not-available', control.errPage)
 
 //==========================================================================================
 
-module.exports = router ;
+module.exports = router;
